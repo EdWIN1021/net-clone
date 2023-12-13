f339008d5f7adc1098f8822091e318e90019c345
Edwin <>
2023-12-12 19:55:05 -0800

    feat: add DevicesSection

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,9 +1,13 @@
 import BannerSection from "../components/BannerSection";
+import DevicesSection from "../components/DevicesSection";
+import TVSection from "../components/TVSection";
 
 const root = () => {
   return (
-    <main>
+    <main className="bg-[rgb(35,35,35,35)]">
       <BannerSection />
+      <TVSection />
+      <DevicesSection />
     </main>
   );
 };
