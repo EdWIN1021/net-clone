821912d4503705db5c79c72cb360832fe4e8f517
Edwin <>
2023-12-12 20:46:22 -0800

    feat: update

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,13 +3,13 @@ import BannerSection from "../components/BannerSection";
 import DevicesSection from "../components/DevicesSection";
 import KidSection from "../components/KidSection";
 import QuestionSection from "../components/QuestionSection";
-// import TVSection from "../components/TVSection";
+import TVSection from "../components/TVSection";
 
 const root = () => {
   return (
     <main className="bg-[rgb(35,35,35,35)]">
       <BannerSection />
-      {/* <TVSection /> */}
+      <TVSection />
       <DevicesSection />
       <KidSection />
       <AppSection />
